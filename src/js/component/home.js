7f870ce09a1d4cccfdb767b62de82ce9a5df26b7
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-05-02 19:54:14 +0000

    Subida de archios y practica

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [datos, setDatos] = useState({
+		nombre: "",
+		apellido: ""
+	});
+	const handleInputChange = event => {
+		console.log(event.target.value);
+
+		/* Sabe en que lugar del objet posicionarla entre nombre o apellido por el name */
+		setDatos({
+			/* agregamos ...datos, para no sobre escribir, y que guarde lo que va tenienedo al momento. */
+			...datos,
+			/*Nombre_O_Apellido  : Martin_O_coimbra */
+			[event.target.name]: event.target.value
+		});
+	};
+
+	const enviarDatos = event => {
+		event.preventDefault();
+		console.log(
+			datos.nombre + "-" + datos.apellido + "-" + datos.tercerName
+		);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<form onSubmit={enviarDatos} className="text-center mt-5">
+			<h1>Formulario</h1>
+			<div className="row justify-content-center">
+				<div className="col-3 text-right">
+					<input
+						type="text"
+						placeholder="Nombre:"
+						name="nombre"
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div className="col-3">
+					<input
+						type="text"
+						placeholder="Apellido:"
+						name="apellido"
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div className="col-3">
+					<input
+						type="text"
+						placeholder="tercerName:"
+						name="tercerName"
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div className="col-3 text-left">
+					<button className="btn btn-primary" type="submit">
+						Enviar
+					</button>
+				</div>
+			</div>
+			{/* Imprimimos lo que va escribiendo al momento */}
+			<h1>
+				{datos.nombre}-{datos.apellido}-{datos.tercerName}
+			</h1>
+		</form>
 	);
 }
